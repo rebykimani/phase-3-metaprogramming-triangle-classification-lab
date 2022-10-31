3c98ef7f60da7660a8b13992a1ebd1c103304410
rebykimani <beckireby15@gmail.com>
2022-10-31 16:53:36 +0300

    completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  def initialize(a, b,c)
+    @sides = [a,b,c]  
+  end
+
+  def pass_triangle_equality?(sides)
+    a, b, c = sides
+    sides.all? {|n| n > 0} && (a + b > c && a + c > b && b + c > a)
+  end
+
+  def kind
+    if pass_triangle_equality?(@sides)
+      if @sides.all? {|n| n == @sides[1]}
+        :equilateral
+      elsif @sides.uniq.size == 2 
+        :isosceles
+      else
+        :scalene
+      end
+    else       
+      begin
+       raise TriangleError   
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Invalid triangle!"
+    end    
+  end
 end
